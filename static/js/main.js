84c05cda6246ba84f8c0994d0f4291a390e9beaf
matheus-liedson <matheusliedson7@gmail.com>
2023-05-14 14:22:18 -0400

    v1.1

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,7 @@
 const foto1 = new PANOLENS.ImagePanorama( 'static/images/foto1.jpeg' );
 
 const foto2 = new PANOLENS.ImagePanorama('static/images/foto2.jpeg');
-const foto3= new PANOLENS.ImagePanorama('static/images/foto3.jpeg');
+const foto3= new PANOLENS.ImagePanorama('static/images/foto3.jpg');
 const foto4 = new PANOLENS.ImagePanorama('static/images/foto4.jpeg');
 const foto5 = new PANOLENS.ImagePanorama('static/images/foto5.jpeg');
 
@@ -13,8 +13,8 @@ let imageContainer = document.querySelector('.image-container')
 
 var infospotPositions = [
   
-    new THREE.Vector3(-2136.06, 16.30, 890.14),
-    new THREE.Vector3(-3136.06, 296.30, -4290.14),
+    new THREE.Vector3(-3000, -10, 200),
+    new THREE.Vector3(3000, 10, -250),
     new THREE.Vector3(-2000,200,-2000)
     
   ];
@@ -29,10 +29,21 @@ const viewer = new PANOLENS.Viewer({
 
 
 foto1.link(foto2,infospotPositions[0])
-foto2.link(foto1,infospotPositions[0])
-
-foto2.link(foto3,infospotPositions[2])
+foto2.link(foto1,infospotPositions[1])
 
 
-viewer.add( foto1,foto2,foto3 );
+
+foto2.link(foto3,infospotPositions[0])
+foto3.link(foto2,infospotPositions[1])
+
+foto3.link(foto4,infospotPositions[0])
+foto4.link(foto3,infospotPositions[1])
+
+foto4.link(foto5,infospotPositions[0])
+foto5.link(foto4,infospotPositions[1])
+
+
+
+
+viewer.add( foto2,foto1,foto3,foto4,foto5 );
 
